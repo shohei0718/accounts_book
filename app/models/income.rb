65729a2b9ff99b1f09d361b13d07653eb54dadcc
shohei <vagrant@localhost.localdomain>
2019-10-03 15:59:56 +0000

    検索ロジックを作成#11

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,19 @@
 class Income < ApplicationRecord
   belongs_to :income_item
+
+  def self.search(start_date, end_date)
+    if start_date && end_date
+      where(date: start_date..end_date)
+    elsif start_date
+      where(date >= start_date)
+    elsif end_date
+      where(date <= end_date)
+    else #検索がなければ、当月の収入一覧を表示させる
+      this_month
+    end
+  end
+  
+  scope :this_month, -> {
+    where(date: Time.now.all_month).order(date: "DESC")
+  }
 end
