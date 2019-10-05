745c23ed6fc84193ea293e53332a2cff0af60ce1
shohei <vagrant@localhost.localdomain>
2019-10-05 14:34:28 +0000

    コールバックでユーザーがログインしているかどうかをチェック#5

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,14 @@
 class TopController < ApplicationController
+  before_action :login_check
   def index
+  end
+
+  private
+  
+  def login_check
+    unless logged_in?
+      flash[:alert] = 'ログインしてください'
+      redirect_to login_path
+    end
   end
 end
