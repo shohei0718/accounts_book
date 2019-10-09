ee687c417afb29af5ff3a4e258d61d44f55713d7
shohei <vagrant@localhost.localdomain>
2019-10-09 15:30:29 +0000

    generatorで不要なファイルを作成しないようにした#1

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module AccountsBook
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    
+    config.generators do |g|
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
